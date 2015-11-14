50f91db0d821af628bdff9e4dddf78328a4bce95
Jason Yu <jasonyu@fas.harvard.edu>
2015-11-13 23:13:00 -0500

    Added Hashtags table migration and seeder.

---
+++ database/migrations/2015_11_13_230000_create_hashtags_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHashtagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hashtags', function (Blueprint $table) {
+            // make a Primary, Auto-Incrementing field
+            $table->increments('id');
+
+            // make two columns: `created_at` and `updated_at` to keep track of
+            // changes to a row
+            $table->timestamps();
+
+            // make the rest of the columns...
+            $table->string('term');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('hashtags');
+    }
+}
