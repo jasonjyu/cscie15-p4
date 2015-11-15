b60f1c2e366815294788beed7f2fb725b6e19dc8
Jason Yu <jasonyu@fas.harvard.edu>
2015-11-15 16:20:00 -0500

    Fixed typo in Search view.

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -32,8 +32,8 @@ specific styesheets.
         </ul>
     @endif
 
-    {{-- if the $twitter_results array is set, then print out the tweets --}}
-    @if (isset($twitter_results) && !empty($twitter_results))
+    {{-- if $twitter_results is not empty, then print out the tweets --}}
+    @if (!empty($twitter_results))
         <div class='twitter-results'>
             <h3>Twitter</h3>
             @foreach ($twitter_results as $tweet)
@@ -46,8 +46,8 @@ specific styesheets.
         </div>
     @endif
 
-    {{-- if the $twitter_results array is set, then print out the tweets --}}
-    @if (isset($instagram_results) && !empty($instagram_results))
+    {{-- if $instagram_results is not empty, then print out the posts --}}
+    @if (!empty($instagram_results))
         <div class='instagram-results'>
             <h3>Instagram</h3>
             @foreach ($instagram_results as $post)
