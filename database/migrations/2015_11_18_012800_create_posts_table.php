f8e4ba0fca9b7d2fc3fedbde7e9443cd1d653ce1
Jason Yu <jasonyu@fas.harvard.edu>
2015-11-18 02:10:00 -0500

    Added Posts table migration and seeder.

---
+++ database/migrations/2015_11_18_012800_create_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            // make a Primary, Auto-Incrementing field
+            $table->increments('id');
+
+            // make two columns: `created_at` and `updated_at` to keep track of
+            // changes to a row
+            $table->timestamps();
+
+            // make the rest of the columns...
+            $table->string('feed');
+            $table->timestamp('posted_at');
+            $table->string('text');
+            $table->string('uri');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
